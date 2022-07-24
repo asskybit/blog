79b244052df41ea0a3aca7c366ecf86868d7234c
Alexander Shidlovsky <alexandershidlovskyby@gmail.com>
2022-07-24 22:31:58 +0300

    Create blog unlike button

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x4ns&6n(v=fr@60g&!%r!(_jpz23t6w-57a49tbv#7usjs4*0y'
+from secret_key import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
