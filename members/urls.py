b8a0e47c075d3145bd1910b7c3b3b1d6a696ffad
Alexander Shidlovsky <alexandershidlovskyby@gmail.com>
2022-07-26 04:08:44 +0300

    create edit profile page

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditForm
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit_profile/', UserEditForm.as_view(), name='edit_profile'),
 ]
